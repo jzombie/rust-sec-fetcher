5734ce374064e3dfc5ece268e9d33a0a5b80557a
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-03-06 20:46:50 -0600

    Update tests

--- tests/human_readable_us_gaap_mapping_tests.rs
+++ tests/human_readable_us_gaap_mapping_tests.rs
@@ -13,130 +13,115 @@ fn test_assets() {
     );
 }
 
-// #[test]
-// fn test_benefits_costs_expenses() {
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping("BenefitsLossesAndExpenses")
-//             .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from(["BenefitsCostsExpenses", "CostsAndExpenses"]))
-//     );
-// }
+#[test]
+fn test_benefits_costs_expenses() {
+    assert_eq!(
+        get_us_gaap_human_readable_mapping("BenefitsLossesAndExpenses"),
+        Some(vec!["BenefitsCostsExpenses", "CostsAndExpenses"])
+    );
+}
 
-// #[test]
-// fn test_commitments_and_contingencies() {
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping("CommitmentsAndContingencies")
-//             .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from(["CommitmentsAndContingencies"]))
-//     );
-// }
+#[test]
+fn test_commitments_and_contingencies() {
+    assert_eq!(
+        get_us_gaap_human_readable_mapping("CommitmentsAndContingencies"),
+        Some(vec!["CommitmentsAndContingencies"])
+    );
+}
 
-// #[test]
-// fn test_comprehensive_income_loss() {
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping(
-//             "ComprehensiveIncomeNetOfTaxIncludingPortionAttributableToNoncontrollingInterest"
-//         )
-//         .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from(["ComprehensiveIncomeLoss"]))
-//     );
+#[test]
+fn test_comprehensive_income_loss() {
+    assert_eq!(
+        get_us_gaap_human_readable_mapping(
+            "ComprehensiveIncomeNetOfTaxIncludingPortionAttributableToNoncontrollingInterest"
+        ),
+        Some(vec!["ComprehensiveIncomeLoss"])
+    );
 
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping("ComprehensiveIncomeNetOfTax")
-//             .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from([
-//             "ComprehensiveIncomeLoss",
-//             "ComprehensiveIncomeLossAttributableToParent"
-//         ]))
-//     );
-// }
+    assert_eq!(
+        get_us_gaap_human_readable_mapping("ComprehensiveIncomeNetOfTax"),
+        Some(vec![
+            "ComprehensiveIncomeLoss",
+            "ComprehensiveIncomeLossAttributableToParent"
+        ])
+    );
+}
 
-// #[test]
-// fn test_comprehensive_income_loss_attributable_to_noncontrolling_interest() {
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping(
-//             "ComprehensiveIncomeNetOfTaxAttributableToNoncontrollingInterest"
-//         )
-//         .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from([
-//             "ComprehensiveIncomeLossAttributableToNoncontrollingInterest"
-//         ]))
-//     );
-// }
+#[test]
+fn test_comprehensive_income_loss_attributable_to_noncontrolling_interest() {
+    assert_eq!(
+        get_us_gaap_human_readable_mapping(
+            "ComprehensiveIncomeNetOfTaxAttributableToNoncontrollingInterest"
+        ),
+        Some(vec![
+            "ComprehensiveIncomeLossAttributableToNoncontrollingInterest"
+        ])
+    );
+}
 
-// #[test]
-// fn test_comprehensive_income_loss_attributable_to_parent() {
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping("ComprehensiveIncomeNetOfTax")
-//             .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from([
-//             "ComprehensiveIncomeLossAttributableToParent",
-//             "ComprehensiveIncomeLoss"
-//         ]))
-//     );
-// }
+#[test]
+fn test_comprehensive_income_loss_attributable_to_parent() {
+    assert_eq!(
+        get_us_gaap_human_readable_mapping("ComprehensiveIncomeNetOfTax"),
+        Some(vec![
+            "ComprehensiveIncomeLoss",
+            "ComprehensiveIncomeLossAttributableToParent",
+        ])
+    );
+}
 
-// #[test]
-// fn test_cost_of_revenue() {
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping("CostOfRevenue")
-//             .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from(["CostOfRevenue", "CostsAndExpenses"]))
-//     );
+#[test]
+fn test_cost_of_revenue() {
+    assert_eq!(
+        get_us_gaap_human_readable_mapping("CostOfRevenue"),
+        Some(vec!["CostOfRevenue"])
+    );
 
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping("CostOfGoodsAndServicesSold")
-//             .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from(["CostOfRevenue"]))
-//     );
+    assert_eq!(
+        get_us_gaap_human_readable_mapping("CostOfGoodsAndServicesSold"),
+        Some(vec!["CostOfRevenue"])
+    );
 
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping("CostOfServices")
-//             .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from(["CostOfRevenue"]))
-//     );
+    assert_eq!(
+        get_us_gaap_human_readable_mapping("CostOfServices"),
+        Some(vec!["CostOfRevenue"])
+    );
 
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping("CostOfGoodsSold")
-//             .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from(["CostOfRevenue"]))
-//     );
+    assert_eq!(
+        get_us_gaap_human_readable_mapping("CostOfGoodsSold"),
+        Some(vec!["CostOfRevenue"])
+    );
 
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping(
-//             "CostOfGoodsSoldExcludingDepreciationDepletionAndAmortization"
-//         )
-//         .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from(["CostOfRevenue"]))
-//     );
+    assert_eq!(
+        get_us_gaap_human_readable_mapping(
+            "CostOfGoodsSoldExcludingDepreciationDepletionAndAmortization"
+        ),
+        Some(vec!["CostOfRevenue"])
+    );
 
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping("CostOfGoodsSoldElectric")
-//             .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from(["CostOfRevenue"]))
-//     );
-// }
+    assert_eq!(
+        get_us_gaap_human_readable_mapping("CostOfGoodsSoldElectric"),
+        Some(vec!["CostOfRevenue"])
+    );
+}
 
-// #[test]
-// fn test_costs_and_expenses() {
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping("CostsAndExpenses")
-//             .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from(["CostsAndExpenses"]))
-//     );
+#[test]
+fn test_costs_and_expenses() {
+    assert_eq!(
+        get_us_gaap_human_readable_mapping("CostsAndExpenses"),
+        Some(vec!["CostsAndExpenses"])
+    );
 
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping("BenefitsLossesAndExpenses")
-//             .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from(["CostsAndExpenses", "BenefitsCostsExpenses"]))
-//     );
-// }
+    assert_eq!(
+        get_us_gaap_human_readable_mapping("BenefitsLossesAndExpenses"),
+        Some(vec!["BenefitsCostsExpenses", "CostsAndExpenses"])
+    );
+}
 
-// #[test]
-// fn test_current_assets() {
-//     assert_eq!(
-//         get_us_gaap_human_readable_mapping("AssetsCurrent")
-//             .map(|v| v.into_iter().collect::<HashSet<_>>()),
-//         Some(HashSet::from(["CurrentAssets", "Assets"]))
-//     );
-// }
+#[test]
+fn test_current_assets() {
+    assert_eq!(
+        get_us_gaap_human_readable_mapping("AssetsCurrent"),
+        Some(vec!["Assets", "CurrentAssets"])
+    );
+}
