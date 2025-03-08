18a1439b0c02741749ae0a079837063212fd7360
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-03-08 16:00:13 -0600

    Add `is_development_mode` util

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,8 @@
 mod invert_multivalue_indexmap;
 pub use invert_multivalue_indexmap::invert_multivalue_indexmap;
 
+mod is_development_mode;
+pub use is_development_mode::is_development_mode;
+
 mod is_interactive_mode;
 pub use is_interactive_mode::is_interactive_mode;
