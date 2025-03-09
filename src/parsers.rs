93e34edded5d00440c7662f4e41cf85aee350f53
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-03-08 21:00:46 -0600

    Extract `US-GAAP` parser

--- src/parsers.rs
+++ src/parsers.rs
@@ -3,3 +3,6 @@ pub use parse_nport_xml::parse_nport_xml;
 
 mod parse_investment_companies_csv;
 pub use parse_investment_companies_csv::parse_investment_companies_csv;
+
+mod parse_us_gaap_fundamentals;
+pub use parse_us_gaap_fundamentals::parse_us_gaap_fundamentals;
