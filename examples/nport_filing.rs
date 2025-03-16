f0237cf863a3cff4577903958b83b50717258792
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-03-16 18:12:09 -0500

    Prototype direct fetch of `NPORT` filing

--- examples/nport_filing.rs
+++ examples/nport_filing.rs
@@ -1,21 +1,44 @@
 use sec_fetcher::config::ConfigManager;
-use sec_fetcher::{
-    models::{AccessionNumber, Cik},
-    network::{fetch_nport_filing_by_cik_and_accession_number, SecClient},
+use sec_fetcher::network::{
+    fetch_cik_by_ticker_symbol, fetch_cik_submissions,
+    fetch_nport_filing_by_cik_and_accession_number, CikSubmission, SecClient,
 };
+use std::env;
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Usage: {} <TICKER_SYMBOL>", args[0]);
+        std::process::exit(1);
+    }
+
+    let ticker_symbol = &args[1];
+
     let config_manager = ConfigManager::load()?;
     let client = SecClient::from_config_manager(&config_manager)?;
 
-    let cik_u64 = 884394; // Example CIK
-    let cik = Cik::from_u64(cik_u64)?;
-    let accession_number_str = "000175272425043826"; // Example accession number
-    let accession_number = AccessionNumber::from_str(accession_number_str)?;
+    let cik = fetch_cik_by_ticker_symbol(&client, ticker_symbol)
+        .await
+        .ok();
 
-    let investments = fetch_nport_filing_by_cik_and_accession_number(&client, cik, accession_number).await?;
+    // let cik_u64 = 884394; // Example CIK
+    // let cik = Cik::from_u64(cik_u64)?;
+    // let accession_number_str = "000175272425043826"; // Example accession number
+    // let accession_number = AccessionNumber::from_str(accession_number_str)?;
+
+    let cik_submissions = fetch_cik_submissions(&client, cik.unwrap()).await?;
+
+    let latest_nport_p_submission =
+        CikSubmission::most_recent_nport_p_submission(cik_submissions.as_slice()).unwrap();
+
+    let investments = fetch_nport_filing_by_cik_and_accession_number(
+        &client,
+        latest_nport_p_submission.cik,
+        latest_nport_p_submission.accession_number,
+    )
+    .await?;
 
     for (i, investment) in investments.iter().enumerate() {
         println!("Investment {}: {:?}", i + 1, investment);
--- src/accessors/get_fund_cik_by_ticker_symbol.rs
+++ src/accessors/get_fund_cik_by_ticker_symbol.rs
@@ -1,8 +1,12 @@
+use crate::models::{Cik, InvestmentCompany};
 use std::error::Error;
-use crate::models::{InvestmentCompany, Cik};
 
+// TODO: Move directly to `InvestmentCompany`
 // TODO: Document
-pub fn get_fund_cik_by_ticker_symbol(investment_companies: &[InvestmentCompany], ticker_symbol: &str) -> Result<Cik, Box<dyn Error>> {
+pub fn get_fund_cik_by_ticker_symbol(
+    investment_companies: &[InvestmentCompany],
+    ticker_symbol: &str,
+) -> Result<Cik, Box<dyn Error>> {
     for result in investment_companies.iter() {
         if result.class_ticker == Some(ticker_symbol.to_string()) {
             if let Some(cik_str) = &result.cik_number {
