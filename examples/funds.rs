327b78b756c2b5a52576a7c4bff385735277a2ec
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-03-19 13:35:49 -0500

    Add TODO

--- examples/funds.rs
+++ examples/funds.rs
@@ -8,6 +8,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let config_manager = ConfigManager::load()?;
     let client = SecClient::from_config_manager(&config_manager)?;
 
+    // TODO: Don't hardcode year
     let funds = fetch_investment_company_series_and_class_dataset(&client, 2024).await?;
 
     for fund in funds {
