0198b9a4da5a71f40ec85158932a5a6bf09db744
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-06-25 08:23:55 -0600

    Prototype CLI

--- python/narrative_stack/main.py
+++ python/narrative_stack/main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from narrative-stack!")
+import typer
+from config import project_paths
+from dataclasses import asdict
 
+# https://pypi.org/project/typer/
+app = typer.Typer()
+
+@app.command()
+def paths():
+    print("")
+    for key, path in asdict(project_paths).items():
+        if key == "python_root":
+            continue
+        
+
+        print(f"{key}\t{path}")
+    print("")
+
+# @app.command()
+# def preprocess(stage: int):
+#     if stage == 1:
+#         print("stage 1..")
+    
+#     else:
+#         raise NotImplementedError(f"Stage {stage} not implemented")
 
 if __name__ == "__main__":
-    main()
+    app()
