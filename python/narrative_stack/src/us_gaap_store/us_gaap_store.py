053760f2cfc83a5476a38c09e83de74a10395e89
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-08-01 02:43:15 -0500

    Prototype model config

--- python/narrative_stack/src/us_gaap_store/us_gaap_store.py
+++ python/narrative_stack/src/us_gaap_store/us_gaap_store.py
@@ -9,7 +9,7 @@ from collections import defaultdict
 from tqdm import tqdm
 from pydantic import BaseModel, Field, ConfigDict
 import numpy as np
-from sklearn.preprocessing import QuantileTransformer, StandardScaler
+from sklearn.preprocessing import StandardScaler # QuantileTransformer
 from sklearn.decomposition import PCA
 from typing import Tuple, Iterator, Optional, Any, DefaultDict
 from utils.pytorch import model_hash, get_device, seed_everything
